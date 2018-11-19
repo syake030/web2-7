a7e1dad90f1fa2d2f97bd99b44e791461ee9a245
syake030 <38420660+syake030@users.noreply.github.com>
2018-11-19 18:57:26 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,4 +1,4 @@
-var number = math.floor(math.random()*3);
+var number = Math.floor(Math.random()*3);
 var answer = parseInt(window.prompt('数あてゲーム。０～２の数字を入力してください！'));
 
 var message;
@@ -6,10 +6,10 @@ if(answer === number){
   message = 'あたり!';
 }else if(answer < number){
   message = '残念!大きいです';
-}else if(answer  number){
+}else if(answer > number){
   message = '残念！小さいです';
 }else{
   message = '0~3の数字を入力してください';
 }
 
-docment.getElementById('choice').textContent = message;
+document.getElementById('choice').textContent = message;
